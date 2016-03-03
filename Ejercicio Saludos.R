8c6bd35ebbb86dc006d51b0d96c6321c4ea4a7cf
Diego Vera <flair144@hotmail.com>
2016-03-03 16:06:52 -0600

    Ejercicio de mensaje de saludos
    30 % hola, 20% que onda, 10% que pedo, 20% que pex, 20% que hubo

---
+++ Ejercicio Saludos.R
@@ -0,0 +1,13 @@
+#Saludos
+m1 <- 10*runif(1)
+if (m1[1]<3){
+    x <- "hola"} else if(m1[1]<5){
+        x <- "que onda"
+    } else if (m1[1]<6){
+        x <- "que pedo"
+    } else if (m1[1]<8){
+        x <- "que pex"
+    } else if (m1[1]<10){
+        x <- "que hubo"
+    }
+x
