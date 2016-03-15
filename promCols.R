6bdad18e8fc8128ab2eebbd95d34ed13b8130405
Diego Vera <flair144@hotmail.com>
2016-03-15 15:43:19 -0600

    '
    '

--- promCols.R
+++ promCols.R
@@ -1,6 +1,10 @@
 airquality
-promedioCols <- function(x){
-    y <- mean(airquality[,c(x)])
-    y
+promedioCols <- function(x, na.rm=TRUE){
+    l <- ncol(x)
+    result <- vector("numeric", l)
+    for (i in 1:l) {
+        result[i] <- mean(x[,i], na.rm=na.rm)
+    }
+    result
 }
-promedioCols()
+promedioCols(airquality)
